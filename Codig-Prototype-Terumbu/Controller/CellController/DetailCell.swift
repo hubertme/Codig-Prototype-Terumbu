e26879691d0b08c3855be055b87b1ae90df0f7fc
Hubert Wang <hubert.tatra@gmail.com>
2018-12-05 16:11:32 +0700

    Add detail cell skeleton

---
+++ Codig-Prototype-Terumbu/Controller/CellController/DetailCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailCell.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 05/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class DetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
