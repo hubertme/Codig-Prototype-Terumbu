960c6c374e6de30631525223678e90f7d7502bf6
Hubert Wang <hubert.tatra@gmail.com>
2018-12-05 14:40:00 +0700

    Add Donation model

---
+++ Codig-Prototype-Terumbu/Model/Donation.swift
@@ -0,0 +1,15 @@
+//
+//  Donation.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 05/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+
+class Donation{
+    var title: String = ""
+    var description: String = ""
+    var date: Date?
+}
