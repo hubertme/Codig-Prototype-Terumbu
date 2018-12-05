f876373bc624f5103c0e21fa2e47b2a8757015b9
Hubert Wang <hubert.tatra@gmail.com>
2018-12-05 14:05:14 +0700

    Hijack storyboard + create tab bar raw file
    Use XIB instead

--- Codig-Prototype-Terumbu/AppDelegate.swift
+++ Codig-Prototype-Terumbu/AppDelegate.swift
@@ -13,9 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        let rootVC = ViewController()
+        let navController = UINavigationController(rootViewController: rootVC)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
---
+++ Codig-Prototype-Terumbu/Controller/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarController.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 05/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
