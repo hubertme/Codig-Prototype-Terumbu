fa260fe6c66b06a03bb5454e1bd8808f2a2886b8
Hubert Wang <hubert.tatra@gmail.com>
2018-12-05 15:39:46 +0700

    Add DetailVC skeleton

---
+++ Codig-Prototype-Terumbu/Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 05/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
