b29a64a443cce22476e61a4064f8cd85b6a33ab3
Hubert Wang <hubert.tatra@gmail.com>
2018-12-05 13:51:31 +0700

    Initial Commit

---
+++ Codig-Prototype-Terumbu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 05/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
