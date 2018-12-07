cb36bc6180d6072f3ebbdeb85a95527938537053
Hubert Wang <hubert.tatra@gmail.com>
2018-12-07 17:36:29 +0700

    Add NewsCardCell files

---
+++ Codig-Prototype-Terumbu/Controller/CollectionCellController/NewsCardCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsCardCell.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 07/12/2018.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class NewsCardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
