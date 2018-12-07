3881fc937bf87a34042901b008a5631b745d97f4
Hubert Wang <hubert.tatra@gmail.com>
2018-12-07 14:05:07 +0700

    Add default bar tint colour

---
+++ Codig-Prototype-Terumbu/Resources/GlobalStyle.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalStyle.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 07/12/2018.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import ChameleonFramework
+
+let barTintColour = UIColor.flatSkyBlueColorDark()
