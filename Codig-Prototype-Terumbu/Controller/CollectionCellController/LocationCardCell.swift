3dcf426a90db1cb3f3e3da3ae347a3a6433dfbd8
Hubert Wang <hubert.tatra@gmail.com>
2018-12-07 22:16:17 +0700

    Add location card cell raw files

---
+++ Codig-Prototype-Terumbu/Controller/CollectionCellController/LocationCardCell.swift
@@ -0,0 +1,18 @@
+//
+//  LocationCardCell.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 07/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class LocationCardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
