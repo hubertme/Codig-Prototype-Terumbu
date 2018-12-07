780fb2b50672df03d7a415aa6ca75c0d241a3631
Hubert Wang <hubert.tatra@gmail.com>
2018-12-07 13:08:58 +0700

    Add model for report
    Report for every campaign

---
+++ Codig-Prototype-Terumbu/Model/Report.swift
@@ -0,0 +1,17 @@
+//
+//  Report.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 07/12/2018.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Report {
+    var title: String = ""
+    var description: String = ""
+    var image = UIImage()
+    var date: Date = Date()
+}
