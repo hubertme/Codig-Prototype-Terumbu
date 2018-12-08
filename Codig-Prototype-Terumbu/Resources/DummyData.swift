e91004dbc76cff299c7d7ed688775ff0ea7e96cd
Hubert Wang <hubert.tatra@gmail.com>
2018-12-08 17:42:37 +0700

    Add separate dummy data file

---
+++ Codig-Prototype-Terumbu/Resources/DummyData.swift
@@ -0,0 +1,13 @@
+//
+//  DummyData.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 08/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+
+var dummyCampaigns: Array<Campaign> = []
+
+
