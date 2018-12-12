b24af569ac6cd4324644e94c6124cd0de3998ffd
Hubert Wang <hubert.tatra@gmail.com>
2018-12-12 15:57:07 +0700

    Add news model
    Preparation for RDBMS

---
+++ Codig-Prototype-Terumbu/Model/News.swift
@@ -0,0 +1,17 @@
+//
+//  News.swift
+//  Codig-Prototype-Terumbu
+//
+//  Created by Hubert Wang on 12/12/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class News{
+    var title: String = ""
+    var content: String = ""
+    var newsImage = UIImage()
+    var date = Date()
+}
